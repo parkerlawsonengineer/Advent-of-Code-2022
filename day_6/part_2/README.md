b29e0f2eda21cf36135bfefead9525a0760459e6
parkerlawsonengineer <contact@parkerlawsonengineer.com>
2022-12-17 13:16:28 -0700

    Completed day 6

---
+++ day_6/part_2/README.md
@@ -0,0 +1,18 @@
+# Day 6 (part 2)
+
+## Compilation:
+
+Compile with any c++ compiler, I use g++:
+
+    g++ main.cpp -o day6
+
+## Usage:
+
+from the command line, run the executable followed by the path to a .txt file that contains the input number values. if the .txt is in the same directory as the executable, no path is required
+
+    ./day6 input.txt
+
+The program expects an input in the form of 1 line of characters, representing a datastream buffer. The program moves through the string 14 characters at a time, looking for the message marker: a sequence of 14 unique characters.
+
+The result will be printed at the end of this process, and represents the number of characters that were processed to find the packet.
+
---
+++ day_6/part_2/main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+#define PACKET_LENGTH 14
+
+/*
+*   Input: a string representing 1 'packet' of length PACKET_LENGTH
+*   Returns: boolean representing whether there are any repeating characters in the packet
+*/
+bool tryPacket(string packet) {
+
+    for (int i = 0; i < packet.length(); i++ ) {
+
+        for (int j = i + 1; j < packet.length(); j++) {
+            if (packet[i] == packet[j]) {
+                return false;
+            }
+        }
+
+    }
+
+    return true;
+
+}
+
+/*
+*   Input: string representing a datastream buffer
+*   Returns: number of characters that must be checked before a valid packet was found
+*/
+int getPacketPosition(string line) {
+
+    int pos = 0;
+
+    while ((pos + PACKET_LENGTH) <= line.length()) {
+
+        string packet = line.substr(pos, PACKET_LENGTH);
+        if (tryPacket(packet)) {
+            return pos + PACKET_LENGTH;
+
+        } else {
+            pos++;
+        }
+
+
+    }
+
+    throw invalid_argument("no valid packet found.");
+
+}
+
+int main(int argc, char** argv) {
+
+    //check for arguments
+    if (argc != 2) {
+        cout << "Incorrect number of arguments. Please specify the name of 1 .txt file to read input from." << endl;
+        return -1;
+    }
+
+    ifstream inputFile(argv[1]);
+
+    //if the file doesn't open correctly, exit.
+    if (!inputFile) {
+        cout << "There was a problem opening the file." << endl;
+        return -1;
+    }
+
+    //initialize variables
+    string readLine;
+    int charsTried = 0;
+
+    while (getline(inputFile, readLine)) {
+       charsTried = getPacketPosition(readLine);
+    }
+
+    cout << "The number of characters tried is: " << charsTried << "." << endl;
+
+    return 0;
+
+
+}
